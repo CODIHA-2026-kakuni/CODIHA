a27b2b731df9aa08b46be55b4620c8f61d833eea
大菅海兎 <kaito0907usagi@gmail.com>
2026-09-09 15:23:23 +0900

    sqlデータベースの作成順を明示．施設の区，読み仮名，GoogleMapへのリンクを追加

--- Application/SQL/020_recycle_location.sql
+++ Application/SQL/020_recycle_location.sql
@@ -8,5 +8,9 @@ CREATE TABLE IF NOT EXISTS recycle_location(
     battery BOOLEAN NOT NULL DEFAULT FALSE COMMENT '充電池を回収可能か指定',
     phone BOOLEAN NOT NULL DEFAULT FALSE COMMENT '携帯電話・タブレットパソコンを回収可能か指定',
     other_electronics BOOLEAN NOT NULL DEFAULT FALSE COMMENT 'その他の電子機器を回収可能か指定',
+    ward VARCHAR(100) NOT NULL COMMENT '区名',
+    reading VARCHAR(255) NOT NULL COMMENT '読み仮名',
+    link TEXT COMMENT '施設の地図URL',
+    INDEX idx_recycle_location_reading (reading), -- 索引の作成
     PRIMARY KEY (id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
